69b43d030a1e163ec998eb73a2a43cddee8ed367
Hedarjaki <haidarzaki2006@gmail.com>
2026-05-28 22:04:04 +0700

    update level presicion input

--- main.pas
+++ main.pas
@@ -1,63 +1,76 @@
 {*
   Program Segitiga made by Hapis Supremacy
   Github : https://github.com/Hapis-Supremacy/Segitiga.git
-  
+
   This project is created for learning purpose
 *}
 
 program triangle;
 var
-        a, b, c : integer;
+  a, b, c : Double;
 
-procedure swap(var x, y: integer);
+function maxVal(x, y: Double): Double;
+begin
+  if x > y then maxVal := x else maxVal := y;
+end;
+
+function isApproxEqual(x, y: Double): Boolean;
+{ Relative tolerance 1%: |x - y| / max(x, y) <= 0.01 }
+begin
+  isApproxEqual := abs(x - y) / maxVal(x, y) <= 0.01;
+end;
+
+procedure swap(var x, y: Double);
 var
-        t: integer;
+  t: Double;
 begin
-        t := x;
-        x := y;
-        y := t;
+  t := x;
+  x := y;
+  y := t;
 end;
 
-procedure sortSide(var a, b, c: integer);
-{ IS: a, b, and c represents each side of the triangle and has been initialized with value. }
-{ Process : This is basically 3 step bubble sort. }
-{ FS: a, b, and c sorted in ascending (a < b < c). }
+procedure sortSide(var a, b, c: Double);
+{ IS: a, b, c sudah diinisialisasi }
+{ Process: 3-step bubble sort }
+{ FS: a <= b <= c }
 begin
-        if a > b then swap(a,b);
-        if b > c then swap(b,c);
-        if a > b then swap(a,b);
+  if a > b then swap(a, b);
+  if b > c then swap(b, c);
+  if a > b then swap(a, b);
 end;
 
 begin
-        writeln('Enter the lengths of each side of the triangle:');
-        readln(a, b, c);
-        sortSide(a, b, c);
-        
-        if (a * b * c <= 0) or (c >= a + b) then
-        begin
-                writeln('The Triangle cant be build from these lengths');
-        end
+  writeln('Enter the lengths of each side of the triangle:');
+  readln(a, b, c);
+  sortSide(a, b, c);
 
-        else
-        begin
-                if (a = b) and (b = c) then
-                begin
-                        writeln('The Triangle is EQUILATERAL');
-                end
+  { Validasi: sisi harus positif dan memenuhi triangle inequality }
+  if (a <= 0) or (b <= 0) or (c <= 0) or (c >= a + b) then
+  begin
+    writeln('The Triangle cant be build from these lengths');
+  end
 
-                else if (a = b) or (b = c) or (a = c) then
-                begin
-                        writeln('The Triangle is ISOSCELES');
-                end
+  else
+  begin
+    { Cek Equilateral dulu (lebih spesifik dari Isosceles) }
+    if isApproxEqual(a, b) and isApproxEqual(b, c) then
+    begin
+      writeln('The Triangle is EQUILATERAL');
+    end
 
-                else if (c * c) = (a * a) + (b * b) then
-                begin
-                        writeln('The Triangle is RIGHT TRIANGLE');
-                end
+    else if isApproxEqual(a, b) or isApproxEqual(b, c) or isApproxEqual(a, c) then
+    begin
+      writeln('The Triangle is ISOSCELES');
+    end
 
-                else
-                begin
-                        writeln('The Triangle is FREE TRIANGLE');
-                end;
-        end;
+    else if (c * c) = (a * a) + (b * b) then
+    begin
+      writeln('The Triangle is RIGHT TRIANGLE');
+    end
+
+    else
+    begin
+      writeln('The Triangle is SCALENE TRIANGLE');
+    end;
+  end;
 end.
